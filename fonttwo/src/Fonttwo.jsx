39e8fde3ba66b1777a8b1bafb870a4d3ce659ad4
Oscar Ayala <oscarayala431@gmail.com>
2024-01-13 18:22:07 -0600

    feat: Initial settings project fonttwo

---
+++ fonttwo/src/Fonttwo.jsx
@@ -0,0 +1,10 @@
+
+const Fonttwo = () => {
+    return (
+        <>
+            <h1>Fonttwo</h1>
+        </>
+    )
+}
+
+export default Fonttwo;
---
+++ fonttwo/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Fonttwo from './Fonttwo.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  //<React.StrictMode>
+    <Fonttwo />
+  //</React.StrictMode>,
+)
